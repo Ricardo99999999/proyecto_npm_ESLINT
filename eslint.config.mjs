b730db418d0ff8fbefb583e1c547dc7aff613745
Ricardo <ricardomartin9922@gmail.com>
2025-08-10 19:45:23 +0200

    Modify comments

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,12 +12,12 @@ export default defineConfig([
       },
     },
     rules: {
-      'no-trailing-spaces': 'error',  //Prohíbe espacios en blanco al final de una línea
-      'indent': ['error', 2],         //Enforce indentación con 2 espacios
-      'semi': ['error', 'always'],    //Exige punto y coma al final de cada sentencia
-      'quotes': ['error', 'single'],  //Obliga a usar comillas simples para strings
-      'space-before-blocks': ['warn', 'always'], //Requiere un espacio antes de una llave de bloque
-      'keyword-spacing': ['error', { before: true, after: true }],  //Exige espacios antes y después de palabras clave (if, else, return, etc.)
+      'no-trailing-spaces': 'error',  //Prohibits trailing whitespace at the end of a line
+      'indent': ['error', 2],         //Requires indentation with 2 spaces
+      'semi': ['error', 'always'],    //Requires a semicolon at the end of each statement
+      'quotes': ['error', 'single'],  //Enforces the use of single quotes for strings
+      'space-before-blocks': ['warn', 'always'], //Requires a space before an opening block brace  
+      'keyword-spacing': ['error', { before: true, after: true }],  //Requires spaces before and after keywords (if, else, return, etc.)
     },
   },
 ]);
